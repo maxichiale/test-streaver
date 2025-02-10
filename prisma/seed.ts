ac584b9f792cb8fbf3e5eca9139a57a71e650945
Maximiliano Chiale <maxi.chiale@gmail.com>
2025-02-10 17:37:52 -0300

    Prisma config

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+//https://www.prisma.io/docs/orm/prisma-migrate/workflows/seeding
+
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json());
+    const users = await fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json());
+
+
+    await Promise.all(users.map(async (user) => {
+        try {
+            await prisma.user.create({
+                data: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    posts: {
+                        create: posts.filter(post => post.userId === user.id).map(post => ({
+                            id: post.id,
+                            title: post.title,
+                            body: post.body,
+                        })),
+                    },
+                },
+            });
+        } catch (error) {
+            console.error(`Failed to create user ${user.id}:`, error);
+        }
+    }));
+
+
+    // for (const user of users) {
+    //     await prisma.user.create({
+    //         data: {
+    //             id: user.id,
+    //             name: user.name,
+    //             email: user.email,
+    //             posts: {
+    //                 create: posts.filter(post => post.userId === user.id).map(post => ({
+    //                     id: post.id,
+    //                     title: post.title,
+    //                     body: post.body,
+    //                 })),
+    //             },
+    //         },
+    //     });
+    // }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+
